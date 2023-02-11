e49f488192da4c7bf0339909463f345f813dbb57
Nov-net <ek9062625020@gmail.com>
2023-02-11 17:41:47 +0300

    feat: stats-service

--- stats-server/src/main/java/ru/practicum/service/StatsService.java
+++ stats-server/src/main/java/ru/practicum/service/StatsService.java
@@ -6,7 +6,9 @@ import ru.practicum.dto.StatsDto;
 import java.util.List;
 
 public interface StatsService {
+
     HitDto saveHit(HitDto hitDto);
+
     List<StatsDto> getStats(String start, String end, List<String> uris, Boolean unique);
 
 }
