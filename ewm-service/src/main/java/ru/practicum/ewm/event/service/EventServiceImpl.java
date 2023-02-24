8e9399756017a7473dfe2bae2e5e69eec3c4648a
Nov-net <ek9062625020@gmail.com>
2023-02-24 18:48:06 +0300

    feat: 3 модуля stats-servise + полный модуль ewm-service

--- ewm-service/src/main/java/ru/practicum/ewm/event/service/EventServiceImpl.java
+++ ewm-service/src/main/java/ru/practicum/ewm/event/service/EventServiceImpl.java
@@ -9,7 +9,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.ewm.category.repository.CategoryRepository;
 import ru.practicum.client.StatsClient;
 import ru.practicum.dto.HitDto;
