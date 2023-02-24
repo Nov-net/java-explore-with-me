8d7673ce7a2cfcb79cdc352afbebe5ec843d64ef
Nov-net <ek9062625020@gmail.com>
2023-02-24 13:57:29 +0300

    feat: 3 модуля stats-servise + полный модуль ewm-service

--- ewm-service/src/main/java/ru/practicum/ewm/validator/Validator.java
+++ ewm-service/src/main/java/ru/practicum/ewm/validator/Validator.java
@@ -89,7 +89,7 @@ public class Validator {
 
     @SneakyThrows
     public static boolean checkParticipantLimit(Event event) {
-        if (event.getParticipantLimit() != 0 && event.getParticipantLimit().equals(event.getConfirmedRequests())) {
+        if (!event.getParticipantLimit().equals(0) && event.getConfirmedRequests().equals(event.getParticipantLimit())) {
             log.info("Достигнут предел количества участников: {} из {}", event.getConfirmedRequests(), event.getParticipantLimit());
             throw new ForbiddenException("CONFLICT", "For the requested operation the conditions are not met.",
                     "The participant limit has been reached");
