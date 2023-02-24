238f39c4bcedac1a0f3b33d09eb8a68ba33650c7
Nov-net <ek9062625020@gmail.com>
2023-02-24 13:40:38 +0300

    feat: 3 модуля stats-servise + полный модуль ewm-service

--- stats-service/client/src/main/java/ru/practicum/client/BaseClient.java
+++ stats-service/client/src/main/java/ru/practicum/client/BaseClient.java
@@ -5,6 +5,7 @@ import org.springframework.lang.Nullable;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
 import java.util.Map;
 
 public class BaseClient {
@@ -32,7 +33,8 @@ public class BaseClient {
 
     private <T> ResponseEntity<Object> makeAndSendRequest(HttpMethod method, String path,
                                                           @Nullable Map<String, Object> parameters, @Nullable T body) {
-        HttpEntity<T> requestEntity = new HttpEntity<>(body);
+
+        HttpEntity<T> requestEntity = new HttpEntity<>(body, defaultHeaders(path));
 
         ResponseEntity<Object> statsServerResponse;
         try {
@@ -59,6 +61,16 @@ public class BaseClient {
         }
 
         return responseBuilder.build();
+    }
+
+    private HttpHeaders defaultHeaders(String path) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        if (path != null) {
+            headers.set("path", path);
+        }
+        return headers;
     }
 
 }
